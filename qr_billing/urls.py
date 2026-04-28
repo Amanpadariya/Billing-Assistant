45169ad7ccfc0ff1ac2694d5a5152d579df060e0
Aman Padariya <amanpadariya2003@example.com>
2026-04-28 10:52:57 +0530

    Redirect root URL to login page

--- qr_billing/urls.py
+++ qr_billing/urls.py
@@ -3,8 +3,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from staffpanel.views import dashboard
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/account/login/'), name='home'),
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
     path('', dashboard, name='home'),
